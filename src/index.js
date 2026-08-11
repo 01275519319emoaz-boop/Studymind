eace29ac84d0fecf7c4317db35056829d5f4c6ec
01275519319emoaz-boop <01275519319emoaz@gmail.com>
2026-08-11 18:07:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const response = await fetch("https://api.openai.com/v1/responses", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
-"Authorization": Bearer ${env.OPENAI_API_KEY}
+"Authorization": `Bearer ${env.OPENAI_API_KEY}`
 },
 body: JSON.stringify({
 model: "gpt-5-mini",
